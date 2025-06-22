abcc9e9419f59cc2ce44e692ab6993c221fe7c36
Malu <malu.am2009@gmail.cm>
2025-06-22 17:26:34 -0300

    desenvolvimento e ajustes

---
+++ quiz.py
@@ -0,0 +1,81 @@
+import json
+
+with open('perguntas.json', 'r', encoding='utf-8') as arquivo:
+    dados_perguntas = json.load(arquivo)
+    perguntas = dados_perguntas
+
+
+with open('ranking.json', 'r', encoding='utf-8') as arquivo:
+    dados_ranking = json.load(arquivo)
+    ranking = dados_ranking['jogadores']
+
+
+while True:
+    print("===== MENU =====")
+    print("1. Iniciar Quiz")
+    print("2. Mostrar Ranking")
+    print("3. Desenvolvedoras")
+    print("4. Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == '1':
+
+        nome = input("Digite seu nome: ")
+
+        pontuacao = 0
+
+        for pergunta in perguntas:
+            print("" + pergunta['pergunta'])
+            for i, alt in enumerate(pergunta['opcoes']):
+                print(f"{i + 1}. {alt}")
+
+            resposta = input("Digite o número da resposta ou 'sair' para encerrar o quiz: ")
+
+            if resposta.lower() == 'sair':
+                print(f"\nQuiz encerrado por {nome}. Pontuação parcial: {pontuacao} de {len(perguntas)}")
+                break  
+
+
+            try:
+                indice = int(resposta) - 1
+                if pergunta['opcoes'][indice] == pergunta['resposta']:
+                    print("correto!")
+                    pontuacao += 1
+                else:
+                    print(f"Errado! A resposta correta era: {pergunta['resposta']}")
+            except:
+                print("Resposta inválida.")
+
+        print(f"{nome}, sua pontuação foi: {pontuacao} de {len(perguntas)}")
+
+        ranking.append({"nome": nome, "pontuacao": pontuacao})
+
+        with open('ranking.json', 'w', encoding='utf-8') as arquivo:
+            json.dump({"jogadores": ranking}, arquivo, ensure_ascii=False, indent=4)
+
+        print("Sua pontuação foi para o ranking😊!")
+
+    elif opcao == '2':
+        if not ranking:
+            print("\n Ranking vazio!")
+        else:
+            print("\n===== 🏆 RANKING 🏆 =====")
+            ranking_ordenado = sorted(ranking, key=lambda x: x['pontuacao'], reverse=True)
+
+            for i, jogador in enumerate(ranking_ordenado, start=1):
+                print(f"{i}. {jogador['nome']} - {jogador['pontuacao']} ponto(s)")
+
+            print("==========================")
+
+    elif opcao == '3':
+        print("\n===== Desenvolvido por =====")
+        print("💙 Maria Luiza Almeida")
+        print("💚 Yasmin Rodrigues de Morais")
+
+    elif opcao == '4':
+        print("\nFinalizando!")
+        break
+
+    else:
+        print("\n Opção inválida!")
